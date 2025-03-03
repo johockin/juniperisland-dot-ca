58509e9e8128fffa23cacc7b4b392efbb56321ad
Johnny Hockin <johnny@juniperisland.ca>
2025-03-03 10:26:14 -0500

    fix: update build configuration for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,24 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
+    }
+  },
+  build: {
+    target: 'esnext',
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext'
+    }
   },
   server: {
     port: 3000,
